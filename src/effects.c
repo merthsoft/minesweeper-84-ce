200418cd2dd1a56ab1885367bdc2642b015e737c
Shaun McFall <shaunm.mcfall@gmail.com>
2024-06-15 17:54:05 -0400

    Forgot these--just gonna commit straight to main lol

---
+++ src/effects.c
@@ -0,0 +1,26 @@
+#include <graphx.h>
+
+#include "effects.h"
+#include "gfx/gfx.h"
+
+#define base_palette ((uint16_t*)global_palette)
+#define start 0
+#define length (sizeof_global_palette / 2)
+
+void fadeout()
+{
+    for (int step = 255; step >= 0; step--)
+    {
+        for (int i = start; i < start + length; i++)
+            gfx_palette[i] = gfx_Darken(base_palette[i], step);
+    }
+}
+
+void fadein()
+{
+    for (int step = 0; step < 256; step++)
+    {
+        for (int i = start; i < start + length; i++)
+            gfx_palette[i] = gfx_Darken(base_palette[i], step);
+    }
+}
---
+++ src/effects.h
@@ -0,0 +1,9 @@
+#ifndef __EFFECTS_H_
+#define __EFFECTS_H_
+
+#include <graphx.h>
+
+void fadeout();
+void fadein();
+
+#endif
